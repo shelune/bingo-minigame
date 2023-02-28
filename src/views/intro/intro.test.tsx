cff824770a7bd798a0b6c43a28136718528c46ca
shelune <em.hieunguyen@gmail.com>
2023-02-28 17:00:25 +0200

    Minor cleanup and add some basic testing for intro

---
+++ src/views/intro/intro.test.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { screen, act, fireEvent, render } from "@testing-library/react";
+
+import IntroView from "./intro";
+
+describe("IntroView", () => {
+  it("should render", () => {
+    const { container } = render(
+      <IntroView
+        setUsername={jest.fn()}
+        setShowingGame={jest.fn()}
+        username="Stranger"
+      />
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should disable the button if no username", () => {
+    render(
+      <IntroView
+        setUsername={jest.fn()}
+        setShowingGame={jest.fn()}
+        username=""
+      />
+    );
+    expect(screen.getByText("Enter")).toHaveAttribute("disabled");
+  });
+
+  it("should call setUsername when typing in username", () => {
+    const mockSetUsername = jest.fn();
+    render(
+      <IntroView
+        setUsername={mockSetUsername}
+        setShowingGame={jest.fn()}
+        username=""
+      />
+    );
+    fireEvent.change(screen.getByLabelText("Player name"), {
+      target: {
+        value: "Foobar",
+      },
+    });
+    expect(mockSetUsername).toHaveBeenCalled();
+  });
+
+  it("should call setShowingGame when hitting Enter", () => {
+    const mockSetShowingGame = jest.fn();
+    render(
+      <IntroView
+        setUsername={jest.fn()}
+        setShowingGame={mockSetShowingGame}
+        username="Foobar"
+      />
+    );
+    screen.getByText("Enter").click();
+    expect(mockSetShowingGame).toHaveBeenCalled();
+  });
+});
